cf8fde476ebde036b348b0f1bc577764fd7c39a7
qbnt <quentin.banet@hotmail.fr>
2025-03-24 16:09:32 +0100

    ajout de la gestion d'erreur read et write

--- libasm/srcs/ft_read.s
+++ libasm/srcs/ft_read.s
@@ -1,20 +1,19 @@
 bits 64
 %define STD_READ 0x2000003
+extern ___error
 
 section .text
-	extern ___error
 	global _ft_read
 
 	_ft_read:
 		mov 	rax, STD_READ
 		syscall
+		jc		.error
+		ret
 
-		cmp 	rax, -1
-		jne 	.done
-
-		call	___error
-		mov 	[rax], rcx
-		mov		rax, -1
-
-		.done:
-			ret
+	.error:
+		mov         r15, rax            ; save errno
+		call        ___error            ; retrieve address to errno
+		mov         [rax], r15          ; put errno in return value of __error (pointer to errno)
+		mov         rax, -1
+		ret
--- libasm/srcs/ft_write.s
+++ libasm/srcs/ft_write.s
@@ -1,21 +1,19 @@
 bits 64
 
 %define SYS_WRITE 0x2000004
+extern ___error
 
 section .text
-	extern ___error
 	global _ft_write
 
 	_ft_write:
-		mov     rax, SYS_WRITE
+		mov         rax, SYS_WRITE
 		syscall
-
-		cmp     rax, -1
-		jne     .done
-
-		call    ___error
-		mov     [rax], rcx
-		mov     rax, -1
-
-	.done:
+		jc          .error
+		ret
+	.error:
+		mov         r15, rax
+		call        ___error
+		mov         [rax], r15
+		mov         rax, -1
 		ret
